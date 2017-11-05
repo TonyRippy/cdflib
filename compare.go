613e413c6ffcd86baae2aac7b28dd6847429a195
Tony Rippy <tonyrippy@gmail.com>
2017-11-04 21:06:09 -0400

    Implementation of Min().

--- compare.go
+++ compare.go
@@ -1,5 +1,34 @@
 package cdflib
 
+type min struct {
+	cdfs []CDF
+}
+
+func (m *min) P(x float64) float64 {
+	// What we really want is the probability that one or more values sampled from
+	// the input distributions are <= x. The easiest way to calculate this is to
+	// find the probability that *no* samples are less than x:
+	p := 1.0
+	for _, cdf := range m.cdfs {
+		// P(x) is the probability that a sample from cdf is <= x.
+		// 1 - P(x) is the probability it is > x.
+		p *= 1.0 - cdf.P(x)
+	}
+	// ... and we return the probability that this is not true:
+	return 1.0 - p
+}
+
+func (m *min) Inverse() InverseCDF {
+	return &genericInverse{m}
+}
+
+/*
+Calculates the minimum of a set of random variables.
+*/
+func Min(cdfs ...CDF) CDF {
+	return &min{cdfs}
+}
+
 type max struct {
 	cdfs []CDF
 }
--- compare_test.go
+++ compare_test.go
@@ -4,6 +4,31 @@ import (
 	"testing"
 )
 
+func TestMin(t *testing.T) {
+	cdf1 := Normal(2, 5)
+	cdf2 := Normal(3, 1)
+	m := Min(cdf1, cdf2)
+	checkSanity(m, t)
+
+	// Compute the result by sampling.
+	min := func(a float64, b float64) float64 {
+		if (a < b) {
+			return a
+		}
+		return b
+	}
+	expected, _ := Apply2(cdf1, cdf2, min, 500)
+	samples := UniformSamples(expected, 1000)
+	
+	// Null hypothesis is that the manually computed samples above
+	// were drawn from Min(cdf1,cdf2).
+	const a = 0.001
+	p := 1.0 - KSTest(m, samples)
+	if a < p {
+		t.Errorf("Null hypothesis was rejected. a = %.2f, was %f.", a, p)
+	}
+}
+	
 func TestMax(t *testing.T) {
 	cdf1 := Normal(2, 5)
 	cdf2 := Normal(3, 1)
